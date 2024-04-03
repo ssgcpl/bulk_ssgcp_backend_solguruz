abe7fd8b31675e286d9a5e5ffe6b1ddbe3789437
charmih <charmih@solguruz.com>
2024-04-03 15:08:53 +0530

    Remove cached files in storage/framework/

--- storage/framework/views/859277110ca6c6f36cedfdbb36132a2f528e9f4a.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.4.0/cropper.css">
-<link href="<?php echo e(asset('admin_assets/custom/filepond/css/filepond.css')); ?>" rel="stylesheet">
-<link href="<?php echo e(asset('admin_assets/custom/filepond/css/filepond-plugin-image-preview.css')); ?>" rel="stylesheet">
-<link href="<?php echo e(asset('admin_assets/custom/filepond/css/filepond-plugin-image-edit.css')); ?>" rel="stylesheet"><?php /**PATH /opt/lampp/htdocs/ssgc-bulk/resources/views/layouts/admin/elements/filepond_css.blade.php ENDPATH**/ ?>
