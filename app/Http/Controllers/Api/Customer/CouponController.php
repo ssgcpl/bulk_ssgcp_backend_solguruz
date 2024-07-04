3c811c3241eb3caa8666d6e0319466dcc0b41164
ssgcpl@gmail.com <ssgcpl@gmail.com>
2024-07-04 18:13:45 +0530

    manage 0 amount order as per total payable amount in api

--- app/Http/Controllers/Api/Customer/CouponController.php
+++ app/Http/Controllers/Api/Customer/CouponController.php
@@ -574,6 +574,9 @@ class CouponController extends BaseController
                 'payu','ccavenue'
             ];
             if($cart->total_payable == 0.00){
+            if(!isset($request->payment_method)){
+                    $request->merge(['payment_method' => '0-amount']);
+             }
               $payment_methods[] = '0-amount';
             }
             
